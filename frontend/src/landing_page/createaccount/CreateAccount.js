46fb2c6559537093f0e4a0e52d27f668b4d18252
abhishekpnaik05 <abhishekpnaik05@gmail.com>
2025-06-29 09:44:36 +0530

    backend1

--- frontend/src/landing_page/createaccount/CreateAccount.js
+++ frontend/src/landing_page/createaccount/CreateAccount.js
@@ -18,7 +18,7 @@ const AconovaSignupModal = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('https://aconova-backend.onrender.com/api/createaccount', formData);
+      const response = await axios.post('https://aconova-backend1.onrender.com/api/createaccount', formData);
       alert(response.data.message); // Show success message
       navigate('/'); // Redirect to home or login page after signup
     } catch (error) {
